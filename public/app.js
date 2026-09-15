842539881fd17579de42ac5159250531b7dda0f7
danialalfat07 <danial.alfat07@gmail.com>
2026-09-15 08:27:22 +0700

    Revert "ci: use runner Android SDK"
    This reverts commit 9e5a6007dc197aa6b177c9b3e00f615e6639f2cb.

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "1.9.5";
+const APP_VERSION = "1.9.4";
 /* ============================================================
    Dnialify Project - Dnialify Music Stream - SPA frontend
    Streams via the official YouTube IFrame player, metadata via
