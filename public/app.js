44a96c8967cdd5eb4492b7cbd10ab58a89d63028
danialalfat07 <danial.alfat07@gmail.com>
2026-09-15 08:27:23 +0700

    Revert "chore: bump version to 1.8.7"
    This reverts commit 6d87c8015b2d412362467d966ffcd7a5917870dc.

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "1.8.7";
+const APP_VERSION = "1.8.6";
 /* ============================================================
    Dnialify Project - Dnialify Music Stream - SPA frontend
    Streams via the official YouTube IFrame player, metadata via
