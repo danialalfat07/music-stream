87c41ccb8c1f7cd1d365befcd30199bb74e70e56
danialalfat07 <danial.alfat07@gmail.com>
2026-09-16 07:49:25 +0700

    chore: bump version 2.1.1

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "2.1.0-beta.2";
+const APP_VERSION = "2.1.1";
 const BUILD_CHANNEL = String(APP_VERSION).includes('-beta') ? 'beta' : 'stable';
 window.__BUILD_CHANNEL = BUILD_CHANNEL;
 
