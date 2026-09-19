1601120cf05a93f3228865ee675b2af42f40512d
danialalfat07 <danial.alfat07@gmail.com>
2026-09-19 09:11:09 +0700

    release: v2.3.6

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "2.3.5";
+const APP_VERSION = "2.3.6";
 const BUILD_CHANNEL = String(APP_VERSION).includes('-beta') ? 'beta' : 'stable';
 window.__BUILD_CHANNEL = BUILD_CHANNEL;
 
